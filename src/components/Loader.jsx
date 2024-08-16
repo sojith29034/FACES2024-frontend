ce9c32b25896d6ed3010d6a6e89f3a7485ed130f
Revant Shinde <118761487+Revant-1@users.noreply.github.com>
2024-08-16 19:54:20 +0530

    Update Loader.jsx
    Signed-off-by: Revant Shinde <118761487+Revant-1@users.noreply.github.com>

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,8 @@ import './style/loader.css'; // Optional: For additional styling
 const Loader = () => {
   return (
     <div className="loader-container">
-      <img src="src\assets\Loader.gif" alt="Loading..." className="loader-gif" />
+{/*       <img src="src\assets\Loader.gif" alt="Loading..." className="loader-gif" /> */}
+       <img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExbHZ0b3hkNWJhZzI0enI3MnZ1Ym1wZWFpM3Qwam5wcXFwYXFzbXMzZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/0NOO4C7RWx8eZ7Ed8G/giphy.gif" alt="Loading..." className="loader-gif" />
     </div>
   );
 };
